2255d6d5d730f5d61f83acf2cd1702bfccb731e4
contactcarstestingteam <contactcarstestingteam@gmail.com>
2025-05-12 11:27:36 +0300

    fix rest assured implementation

--- src/main/java/com/contactcars/testcases/HomePageTest.java
+++ src/main/java/com/contactcars/testcases/HomePageTest.java
@@ -1,13 +1,10 @@
 package com.contactcars.testcases;
 
 import com.contactcars.base.TestBase;
-import com.contactcars.pages.HomePage;
-import io.restassured.path.json.JsonPath;
-import org.openqa.selenium.devtools.DevTools;
+import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import io.restassured.RestAssured;
-import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
@@ -26,32 +23,21 @@ public class HomePageTest extends TestBase {
 
         // Specify the base URL to the RESTful web service
         RestAssured.baseURI = "https://fake-json-api.mock.beeceptor.com/users";
-
         // Get the RequestSpecification of the request that is to be sent
         // to the server.
         RequestSpecification httpRequest = RestAssured.given();
-
         // Call RequestSpecification.get() method to get the response.
         // Make sure you specify the resource name.
         Response response = httpRequest.get("/Colby");
-
         String strJson = response.asString();
-        System.out.println(response.asString());
-        // First get the JsonPath object instance from the Response interface
-        JsonPath jsonPath = response.jsonPath();
-//        JsonPath jsonPath = new JsonPath(strJson);
-        System.out.println(jsonPath);
-
-        // Then simply query the JsonPath object to get a String value of the node specified by JsonPath
-//        String id = jsonPath.getString("name");
-
-        // Let us print the city variable to see what we got
-//        System.out.println("ID received from Response " + id);
+        // First get the Json object instance from the Response interface
+        JSONObject responseBodyInJSON = new JSONObject(strJson);
+        // Then simply query the Json object to get a String value of the node specified by JsonObject
+        String id = responseBodyInJSON.getString("id");
+        // Let us print the ID variable to see what we got
+        System.out.println("ID received from Response " + id);
 
         // Validate the response
-//        Assert.assertEquals(id, "Colby", "Correct ID received in the Response");
-
-//        System.out.println("Response Body is =>  " + response.asString());
-
+        Assert.assertEquals(id, "Colby", "Correct ID received in the Response");
     }
 }
