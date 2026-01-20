79053166e33b9979cb6e2e1b49ac43585196701b
Mohamed Abdelraheem <mohamedrahem96@gmail.com>
2026-01-20 13:29:23 +0200

    Enhance Action Utility

--- src/main/java/com/contactcars/utils/ActionUtils.java
+++ src/main/java/com/contactcars/utils/ActionUtils.java
@@ -1,5 +1,6 @@
 package com.contactcars.utils;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -15,19 +16,24 @@ public class ActionUtils {
         this.wait = new WaitUtils(driver, 30);
     }
 
-    public void buttonClick(WebElement element) {
-        wait.waitForPageToLoad(30);
-        wait.waitForElementClickable(element);
+    public void buttonClick(By locator) {
+        WebElement element = wait.waitForElementClickable(locator);
         element.click();
     }
 
-    public void type(WebElement element, String text) {
+    public void type(By locator, String text) {
+        WebElement element = wait.waitForElementVisible(locator);
+        element.clear();
         element.sendKeys(text);
     }
 
-    public void chooseValue(List<WebElement> elements, int index) {
-        wait.waitForAllElementsVisible(elements);
-        elements.get(index).click();
+    public void chooseValue(By locator, String value) {
+        List<WebElement> elements = wait.waitForAllElementsVisible(locator);
+        elements.stream()
+                .filter(e -> e.getText().equalsIgnoreCase(value))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Value not found: " + value))
+                .click();
     }
 
 }
